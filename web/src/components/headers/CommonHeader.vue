41deb7fd91456cdbd8952f239d64d9bfb7a15250
justworld <jiangui1376044@126.com>
2021-02-09 08:20:59 +0800

    feature: init

--- web/src/components/headers/CommonHeader.vue
+++ web/src/components/headers/CommonHeader.vue
@@ -1,19 +1,24 @@
 <template>
   <el-header class="head1">
-    <div class="logo1"></div>
-    <div class="menu1"></div>
-    <div class="search1"></div>
-    <div class="wechat1"></div>
+    <div class="head2">
+      <div class="logo1"></div>
+      <div class="menu1"></div>
+      <div class="search1">
+        <el-input clearable :placeholder="searchHot"></el-input>
+      </div>
+      <div class="wechat1"></div>
+    </div>
   </el-header>
 </template>
 
 <script>
 export default {
+  created () {
+  },
   data () {
     return {
+      searchHot: 'hot'
     }
-  },
-  created () {
   },
   methods: {
   }
